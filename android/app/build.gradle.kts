fd387ce2399ed5bc51a20a6d32b0613600941981
unknown <Administrator@MtplDeveloper1.Mezantea.local>
2026-02-12 16:50:27 +0500

    updtions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 android {
     namespace = "com.example.taskoon_application"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     defaultConfig {
         applicationId = "com.example.taskoon_application"
